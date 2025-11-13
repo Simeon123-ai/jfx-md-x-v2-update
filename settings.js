c060a8bcbb160e55e80104c47e9dfc5ee3a04f83
Simeon123-ai <simeonwada35@gmail.com>
2025-11-13 16:48:23 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'JEPHTER TECH',
   botName: "☞ ᴊꜰx ᴍᴅ-xᴠ2",
   botOwner: 'SIMEON', // Your name
-  ownerNumber: '2347046970033', // No + symbol
+  ownerNumber: '2347054389353', // No + symbol
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
   description: "ᴀ ᴘᴏᴡᴇʀꜰᴜʟ ᴡʜᴀᴛꜱᴀᴘᴘ ᴍᴅ ʙᴏᴛ ʙʏ ᴊᴇᴘʜᴛᴇʀ ᴛᴇᴄʜ. ᴄʜᴀɴɴᴇʟ: https://whatsapp.com/channel/0029VbAxkJl0lwgqAOojKI3R",
